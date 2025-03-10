b089b80f94967eeae7caabcf6ac0662fd414b6e5
bill chipman <wrchipman@gmail.com>
2025-03-10 18:39:04 -0300

    Changed ball spin direction based on player collision layer

--- scenes/ball.gd
+++ scenes/ball.gd
@@ -1,6 +1,9 @@
 extends RigidBody2D
 
-var ball_speed = Vector2(200, 200)
+@export var ball_speed = Vector2(200, 200)
+@export var rotation_speed = 1.5
+var rotation_direction = 1
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,7 +12,11 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
+	rotation += rotation_direction * rotation_speed * delta
 	var collision_info = move_and_collide(ball_speed * delta)
 	if collision_info:
 		ball_speed = ball_speed.bounce(collision_info.get_normal())
+		if collision_info.get_collider().collision_layer == 2:	
+			rotation_direction *= -1
+			
 		
--- scenes/main.gd
+++ scenes/main.gd
@@ -3,23 +3,36 @@ extends Node2D
 var screen_size
 var score1
 var score2
+var ball_start_position
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	screen_size = get_viewport_rect().size
+	print(screen_size)
+	$ColorRect.size = screen_size
+	$ColorRect.position = Vector2.ZERO
+	$Player1.position = Vector2(20, screen_size.y/2)
+	$Player2.position = Vector2(screen_size.x - 20 , screen_size.y/2)
+	ball_start_position = Vector2(screen_size.x/2, screen_size.y/2)
+	$Ball.position = ball_start_position
+	$TopWall.position = Vector2(screen_size.x/2, 0)
+	$TopWall/CollisionShape2D.shape.set_size(Vector2(screen_size.x, 1))
+	$BottomWall.position = Vector2(screen_size.x/2, screen_size.y)
+	$BottomWall/CollisionShape2D.shape.set_size(Vector2(screen_size.x, 1))
+	
 	score1 = 0
 	score2 = 0
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if $Ball.position.x > 330:
+	if $Ball.position.x > screen_size.x:
 		score1 +=1
 		print("Score 1: ", score1)
-		$Ball.position =Vector2(0, 0)
-	if $Ball.position.x < -330:
+		$Ball.position = ball_start_position
+	if $Ball.position.x < 0:
 		score2 +=1
 		print("Score 2: ", score2)
-		$Ball.position =Vector2(0, 0)
+		$Ball.position =ball_start_position
 #	
